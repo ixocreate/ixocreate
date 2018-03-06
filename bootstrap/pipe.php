b6dd467c83e80db47933922139fd1f2ddc712e68
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-03-06 10:49:59 +0100

    composer update

--- bootstrap/pipe.php
+++ bootstrap/pipe.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App;
 
 /** @var \KiwiSuite\ApplicationHttp\Pipe\PipeConfigurator $pipeConfigurator */
+use App\Middleware\TestMiddleware;
 use KiwiSuite\Admin\Middleware\AdminMiddleware;
 use KiwiSuite\ApplicationHttp\Pipe\PipeConfigurator;
 use KiwiSuite\ProjectUri\Middleware\ProjectUriCheckMiddleware;
@@ -12,3 +13,5 @@ $pipeConfigurator->pipe(ProjectUriCheckMiddleware::class);
 $pipeConfigurator->segment('/admin', function (PipeConfigurator $pipeConfigurator) {
     $pipeConfigurator->pipe(AdminMiddleware::class);
 });
+
+$pipeConfigurator->pipe(TestMiddleware::class);
