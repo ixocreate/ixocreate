400dd41799ad03af14b975548fc0d15d56ab41bc
Lucas Seinfeld <l.seinfeld@ixolit.com>
2019-04-20 03:44:34 +0200

    update bootstrap items

--- bootstrap/application.php
+++ bootstrap/application.php
@@ -1,14 +1,12 @@
 <?php
 declare(strict_types=1);
 
-namespace Application;
+namespace App;
 
 use Ixocreate\Admin\Package as AdminPackage;
 use Ixocreate\Application\ApplicationConfigurator;
-use Ixocreate\CommonTypes\Package as CommonTypePackage;
 use Ixocreate\Framework\Package as FrameworkPackage;
 
 /** @var ApplicationConfigurator $application */
-$application->addPackage(CommonTypePackage::class);
 $application->addPackage(FrameworkPackage::class);
 $application->addPackage(AdminPackage::class);
