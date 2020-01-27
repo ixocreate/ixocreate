beba2a31e0b5454b1bdad4f86217afddda029df2
Lucas Seinfeld <l.seinfeld@ixolit.com>
2020-01-27 16:45:15 +0100

    add admin bootstrap default timezone and locale

--- bootstrap/admin.php
+++ bootstrap/admin.php
@@ -25,3 +25,6 @@ $admin->setLoginLogo('https://ixocreate.github.io/current/img/brand/ixocreate/ix
 $admin->setIcon('https://ixocreate.github.io/current/img/brand/ixocreate/favicon/favicon-128.png');
 $admin->setLogo('https://ixocreate.github.io/current/img/brand/ixocreate/ixocreate-logo.svg', 140, 50);
 $admin->setBackground('https://ixocreate.github.io/current/img/brand/ixocreate/ixocreate-features-header-frames0059.jpg');
+
+$admin->setDefaultLocale('en_US'); // set to e.g. de_AT
+$admin->setDefaultTimezone('UTC'); // set to e.g. Europe/Vienna
